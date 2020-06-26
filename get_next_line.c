70518e1eb1ad31465093867cf7a38e207f966771
tayran-mila <tmendes-@student.42sp.org.br>
2020-06-26 07:42:15 -0300

    Uploading the version corrected by norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmendes- <tmendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 13:22:26 by tmendes-          #+#    #+#             */
-/*   Updated: 2020/06/24 15:36:51 by tmendes-         ###   ########.fr       */
+/*   Updated: 2020/06/26 07:38:52 by tmendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,25 @@ static char	*join_ptr(char *dst, char *src)
 }
 
 /*
+** This function shifts the buffer after a newline is found.
+*/
+
+static char	*buffer_shift(char *buffer, int nl)
+{
+	char	*aux;
+
+	if (!(aux = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (NULL);
+	ft_bzero(aux, (BUFFER_SIZE + 1));
+	ft_memcpy(aux, (buffer + nl + 1), ft_strlen(buffer + nl + 1));
+	ft_bzero(buffer, BUFFER_SIZE + 1);
+	ft_memcpy(buffer, aux, ft_strlen(aux));
+	free(aux);
+	aux = NULL;
+	return (buffer);
+}
+
+/*
 ** This function returns a line read from a file descriptor,
 ** without the newline.
 */
@@ -79,7 +98,6 @@ static char	*join_ptr(char *dst, char *src)
 int			get_next_line(int fd, char **line)
 {
 	static char	*buf;
-	char		*aux;
 	int			nl;
 
 	if (fd > MX_FD || fd < 0 || line == NULL || !(*line = ft_strdup("")) ||
@@ -87,20 +105,20 @@ int			get_next_line(int fd, char **line)
 		return (-1);
 	while (detect_nl(buf) == -1)
 	{
-		if (!(*line = join_ptr(*line, buf)) || !(buf = buf_read(fd, buf, -1)))
+		if (!(*line = join_ptr(*line, buf)) ||
+		!(buf = buf_read(fd, buf, -1)))
 			return (-1);
-		if(ft_strlen(buf) == 0)
-			return (0);			
+		if (ft_strlen(buf) == 0)
+		{
+			free(buf);
+			buf = NULL;
+			return (0);
+		}
 	}
 	nl = detect_nl(buf);
 	*(buf + nl) = 0;
-	aux = NULL;
 	if (!(*line = join_ptr(*line, buf)) ||
-	!(aux = buf_read(fd, aux, 0)))
+	!(buf = buffer_shift(buf, nl)))
 		return (-1);
-	ft_memcpy(aux, (buf + nl + 1), ft_strlen(buf + nl + 1));
-	ft_bzero(buf, BUFFER_SIZE + 1);
-	ft_memcpy(buf, aux, ft_strlen(aux));
-	free(aux);
 	return (1);
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tmendes- <tmendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 13:22:26 by tmendes-          #+#    #+#             */
-/*   Updated: 2020/06/24 15:37:46 by tmendes-         ###   ########.fr       */
+/*   Updated: 2020/06/26 07:38:26 by tmendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,25 @@ static char	*join_ptr(char *dst, char *src)
 }
 
 /*
+** This function shifts the buffer after a newline is found.
+*/
+
+static char	*buffer_shift(char *buffer, int nl)
+{
+	char	*aux;
+
+	if (!(aux = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (NULL);
+	ft_bzero(aux, (BUFFER_SIZE + 1));
+	ft_memcpy(aux, (buffer + nl + 1), ft_strlen(buffer + nl + 1));
+	ft_bzero(buffer, BUFFER_SIZE + 1);
+	ft_memcpy(buffer, aux, ft_strlen(aux));
+	free(aux);
+	aux = NULL;
+	return (buffer);
+}
+
+/*
 ** This function returns a line read from a file descriptor,
 ** without the newline.
 */
@@ -79,7 +98,6 @@ static char	*join_ptr(char *dst, char *src)
 int			get_next_line(int fd, char **line)
 {
 	static char	*buf[MX_FD];
-	char		*aux;
 	int			nl;
 
 	if (fd > MX_FD || fd < 0 || line == NULL || !(*line = ft_strdup("")) ||
@@ -90,17 +108,17 @@ int			get_next_line(int fd, char **line)
 		if (!(*line = join_ptr(*line, buf[fd])) ||
 		!(buf[fd] = buf_read(fd, buf[fd], -1)))
 			return (-1);
-		if(ft_strlen(buf[fd]) == 0)
-			return (0);			
+		if (ft_strlen(buf[fd]) == 0)
+		{
+			free(buf[fd]);
+			buf[fd] = NULL;
+			return (0);
+		}
 	}
 	nl = detect_nl(buf[fd]);
 	*(buf[fd] + nl) = 0;
-	aux = NULL;
-	if (!(*line = join_ptr(*line, buf[fd])) || !(aux = buf_read(fd, aux, 0)))
+	if (!(*line = join_ptr(*line, buf[fd])) ||
+	!(buf[fd] = buffer_shift(buf[fd], nl)))
 		return (-1);
-	ft_memcpy(aux, (buf[fd] + nl + 1), ft_strlen(buf[fd] + nl + 1));
-	ft_bzero(buf[fd], BUFFER_SIZE + 1);
-	ft_memcpy(buf[fd], aux, ft_strlen(aux));
-	free(aux);
 	return (1);
 }
